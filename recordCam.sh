4ffca7fe39cf16abc5af8e6a6dc8a2b091acc909
Andrei Finski <andrei_finski@epam.com>
2018-02-25 08:31:25 +0300

    Added cam id to the recording folder

--- recordCam.sh
+++ recordCam.sh
@@ -13,5 +13,5 @@ name="`date +%Y-%m-%d_%H.%M`"
 path='/video-rec'
 
 # Save the streams using ffmpeg at 30 fps, stopping the capture after 900 seconds (15 minutes). Add more lines if you have more than 2 cameras
-ffmpeg -i rtsp://@192.168.0.195/unicast  -c copy -c:a aac -t 900 $path/$name.mp4 </dev/null >/dev/null 2>/tmp/xiaofang_cam1.log
+ffmpeg -i rtsp://@192.168.0.195/unicast  -c copy -c:a aac -t 900 $path/cam01/$name.mp4 </dev/null >/dev/null 2>/tmp/xiaofang_cam1.log
 
